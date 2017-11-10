15d011f3ab40a2eb744ff285150f80568a73dc5b
Jeff Hatcher <wjeffhatcher@gmail.com>
2017-11-10 17:13:57 -0500

    required Sequelize again

--- models/index.js
+++ models/index.js
@@ -2,7 +2,13 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:3000/wikistack');
 const express = require('express');
 
+const Project = sequelize.define('project', {
+    title: Sequelize.STRING,
+    description: Sequelize.TEXT
+  })
+
 module.exports = {
     Sequelize: Sequelize,
     db: db
 }
+
